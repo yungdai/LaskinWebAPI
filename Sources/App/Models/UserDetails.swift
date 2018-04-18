6532ca30742b4f036de09c917859e9b3d8d41a3f
Yung Dai <yungchidai@gmail.com>
2018-04-18 11:29:09 -0400

    Added UserDetails and MatchMakingInfo Models and filled them in.
    The controllers are not done.

---
+++ Sources/App/Models/UserDetails.swift
@@ -0,0 +1,55 @@
+import Vapor
+import FluentPostgreSQL
+
+final class UserDetails: Codable {
+    
+    // Identifies
+    var id: Int?
+    var userID: User.ID
+    
+    // contact Info
+    var emailAddress: String
+    var mobilePhone: String
+    var officePhone: String
+    
+    
+    // accessiblity
+    var requiresAccessibility: Bool
+    var accessibilityNeeds: String
+    
+    // dietary
+    var hasDietaryNeeds: Bool
+    var dietaryNeeds: String
+    
+    // conflicting schools for judge
+    var conflictingSchools: [String]
+    
+    init(userID: User.ID, emailAddress: String = "none", mobilePhone: String = "none", officePhone: String = "none", requiresAccessibility: Bool = false, accessibilityNeeds: String = "non", hasDietaryNeeds: Bool = false, dietaryNeeds: String = "none", conflictingSchools: [String] = []) {
+        
+        self.userID = userID
+        self.emailAddress = emailAddress
+        self.mobilePhone = mobilePhone
+        self.officePhone = officePhone
+        self.requiresAccessibility = requiresAccessibility
+        self.accessibilityNeeds = accessibilityNeeds
+        self.hasDietaryNeeds = hasDietaryNeeds
+        self.dietaryNeeds = dietaryNeeds
+        self.conflictingSchools = conflictingSchools
+    }
+}
+
+extension UserDetails: PostgreSQLModel {}
+extension UserDetails: Content {}
+extension UserDetails: Parameter {}
+extension UserDetails: Migration {}
+extension UserDetails {
+    
+    // add a computered property to acronym to get the user object of the User's owner.  This returns fluent's generic Parent Type
+    var user: Parent<UserDetails, User> {
+        // Users Flurent's parent function to retreive the parent.  This take the keypath of the user reference on the acronym.
+        return parent(\.userID)
+    }
+}
+
+
+
