33d1e7bdc59715e37cce144c9f85910b5261f0ea
Yung Dai <yungchidai@gmail.com>
2018-04-20 19:29:47 -0400

    createUserDetails in webController is work in progress.  Doesn't work yet.

--- Sources/App/Controllers/WebsiteController.swift
+++ Sources/App/Controllers/WebsiteController.swift
@@ -9,6 +9,17 @@ struct WebsiteController: RouteCollection {
         // set up the main index route
         router.get(use: indexHandler)
         router.get("users", User.parameter, use: userHandler)
+        
+        // set up creating User Details
+        // GET the user details page
+        router.get("users", User.parameter, "userDetails", "create", use: createUserDetailsHandler)
+        
+        // POST the data to save
+        router.post("users", User.parameter, "userDetails", "create", use: createUserDetailsPostHandler)
+        
+        // GET the edit User Details page
+        router.get("users", User.parameter, "userDetails", UserDetails.parameter, "edit", use: editUserDetailsHandler)
+        router.post("users", User.parameter, "userDetails", UserDetails.parameter, "edit", use: editUserDetailsPostHandler)
     }
     
     // this is the default where the templates will spawn from
@@ -29,6 +40,109 @@ struct WebsiteController: RouteCollection {
             
             let context = UserContext(title: "User Information", user: user, fullName: "\(user.firstName) \(user.lastName)")
             return try request.leaf().render("user", context)
+        }
+    }
+    
+    // router.get("users", User.parameter, "userDetails", "create", use: createUserDetailsHandler)
+    // CREATE UserDetails handler
+    func createUserDetailsHandler(_ request: Request) throws -> Future<View> {
+        
+        return try request.parameter(User.self).flatMap(to: View.self) { user in
+            
+            let context = CreateUserDetailContext(title: "Create User Details for \(user.firstName) \(user.lastName)", user: user)
+            return try request.leaf().render("createUserDetails", context)
+        }
+    }
+    
+    //  router.post("users", User.parameter, "userDetails", "create", use: createUserDetailsPostHandler)
+    
+    
+//    func createAcronymPostHandler(_ request: Request) throws -> Future<Response> {
+//        return try request.content.decode(AcronynPostData.self).flatMap(to: Response.self) { data in
+//
+//            // create the acronym
+//            let acronym = Acronym(short: data.acronymShort, long: data.acronymLong, creatorID: data.creator)
+//            return acronym.save(on: request).map(to: Response.self) { acronym in
+//                guard let id = acronym.id else {
+//
+//                    // if there is something wrong with the ID go to the homepage for now
+//                    return request.redirect(to: "/")
+//                }
+//
+//                // everything is okay so go to the page of the acronym just created
+//                return request.redirect(to: "/acronyms/\(id)")
+//            }
+//        }
+//    }
+    
+    // CREATE UserDetails POST handler
+    func createUserDetailsPostHandler(_ request: Request) throws -> Future<Response> {
+        
+        return try request.content.decode(UserDetailsPostData.self).flatMap(to: Response.self) { data  in
+
+            
+            let conflictingSchools = data.conflictingSchools.components(separatedBy: ",")
+            // create the UserDetails
+            
+            let userDetails = UserDetails(userID: data.creator,
+                                          emailAddress: data.emailAddress,
+                                          mobilePhone: data.mobilePhone,
+                                          officePhone: data.officePhone,
+                                          requiresAccessibility: data.requiresAccessibility,
+                                          accessibilityNeeds: data.accessibilityNeeds,
+                                          hasDietaryNeeds: data.hasDietaryNeeds,
+                                          dietaryNeeds: data.dietaryNeeds,
+                                          conflictingSchools: conflictingSchools)
+            
+            return userDetails.save(on: request).map(to: Response.self) { userDetails in
+                
+                guard let id = userDetails.id else {
+                    
+                    // send the user back to the home page if the userID is wrong
+                    return request.redirect(to: "/")
+                }
+                
+                // everything is find so go to the page you just created
+                return request.redirect(to: "/users/\(id)")
+            }
+        }
+    }
+    
+    // Edit UserDetails Handler
+    func editUserDetailsHandler(_ request: Request) throws -> Future<View> {
+        return try flatMap(to: View.self, request.parameter(User.self), request.parameter(UserDetails.self)) { user, userDetails in
+    
+            let context = EditUserDetailsContext(title: "Edit User Details", userDetails:userDetails, user: user)
+            return try request.leaf().render("createUserDetails", context)
+        }
+    }
+    
+    // Edit UserDetails Post Handler
+    func editUserDetailsPostHandler(_ request: Request) throws -> Future<Response> {
+        
+        // retrieve the paramater for the UserData, and decode the post data
+        return try flatMap(to: Response.self, request.parameter(User.self), request.parameter(UserDetails.self), request.content.decode(UserDetailsPostData.self)) { user, userDetails, data in
+
+            userDetails.emailAddress = data.emailAddress
+            userDetails.mobilePhone = data.mobilePhone
+            userDetails.officePhone = data.officePhone
+            userDetails.requiresAccessibility = data.requiresAccessibility
+            userDetails.accessibilityNeeds = data.accessibilityNeeds
+            userDetails.hasDietaryNeeds = data.hasDietaryNeeds
+            userDetails.dietaryNeeds = data.dietaryNeeds
+            
+            let conflictingSchools = data.conflictingSchools.components(separatedBy: ",")
+            userDetails.conflictingSchools = conflictingSchools
+        
+            userDetails.userID = data.creator
+            
+            return userDetails.save(on: request).map(to: Response.self) { userDetails in
+                guard let id = userDetails.id else {
+                    return request.redirect(to: "/")
+                }
+                
+                return request.redirect(to: "/users/\(id)")
+            }
         }
     }
 }
@@ -52,5 +166,42 @@ struct UserContext: Codable {
     let title: String
     let user: User
     let fullName: String
+}
+
+
+// This is the first part to creating a Create UserDetail Page
+struct CreateUserDetailContext: Codable {
+    
+    let title: String
+    let user: User
+}
+
+// This is the struct for posting a new UserDetailsData object for creating UserDetails
+struct UserDetailsPostData: Content {
+    
+    // let vapor know the data is from generated page
+    static var defaultMediaType = MediaType.urlEncodedForm
+    
+    let creator: UUID
+    let emailAddress: String
+    let mobilePhone: String
+    let officePhone: String
+    let requiresAccessibility: Bool
+    let accessibilityNeeds: String
+    let hasDietaryNeeds: Bool
+    let dietaryNeeds: String
+    let conflictingSchools: String
+}
+
+// Edit UserDetails struct
+struct EditUserDetailsContext: Codable {
+    
+    let title: String
+    let userDetails: UserDetails
+    let user: User
+    
+    // set editing to true so you can tell the document you're in edit mode
+    let editing = true
+    
 }
 
